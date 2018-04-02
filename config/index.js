26cd3f45cf5a9a60b4287dbfd4aab39b1ca8a4d3
Ben <892156860@qq.com>
2018-04-02 23:22:22 +0800

    提交打包脚本

---
+++ config/index.js
@@ -0,0 +1,23 @@
+const { resolve } = require('path')
+const r = url => resolve(__dirname, url)
+const assetsPath = resolve(process.cwd(), '../../happy-weapp-template')
+
+module.exports = {
+    "json": {
+        "pages": [
+            "pages/index/index"
+        ],
+        "tabBar": {
+            "selectedColor": "#5aaca5",
+            "color": "#565656",
+            "list": []
+        }
+    },
+    "window": {
+        "backgroundTextStyle": "light",
+        "navigationBarBackgroundColor": "#fff",
+        "navigationBarTextStyle": "black"
+    },
+    "assetsPath": assetsPath,
+    "app": r('../app.js')
+}
