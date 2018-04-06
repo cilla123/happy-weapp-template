98eb3c08930bf6b376c549da209e404bd57f5df6
Ben <892156860@qq.com>
2018-04-06 13:22:46 +0800

    修改模板bug

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 import './styles/index.sass'
-import './vendor'
+import './vendor/index.js'
 
 // app.js
 App({
+    
+    onLaunch(){
+        this.getUserInfo()
+    },
+
     async getUserInfo() {
         if (this.globalData.userInfo) return this.globalData.userInfo
 
         const { code } = await wx.loginAsync()
-        const { userInfo } = await wx.getUserInfoAsync()
+        const userInfo = await wx.getUserInfoAsync()
 
+        console.log(userInfo)
         this.globalData.userInfo = userInfo
     },
 
--- vendor/regeneratorRuntime.js
+++ vendor/regeneratorRuntime.js
@@ -1,2 +1,5 @@
+require('./lodash')
+require('./ramda')
+
 import regeneratorRuntime from 'regenerator-runtime'
 global.regeneratorRuntime = regeneratorRuntime
